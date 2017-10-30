8288cfaad789f71e5ad7cd4fcf4d6d5dd1054936
Nick Thomas <nichoth@gmail.com>
2017-10-29 22:22:34 -0700

    first commit

---
+++ src/EVENTS.js
@@ -0,0 +1,12 @@
+var namespace = require('@nichoth/event-utils')
+
+module.exports = namespace({
+    counterList: {
+        view: ['addCounter']
+    }
+
+    counter: {
+        view: ['inc', 'dec']
+    }
+})
+
